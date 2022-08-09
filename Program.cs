8660624f91edba496f39fd6571ded474ea87985d
unknown <gritsunov85@gmail.com>
2022-08-09 23:25:00 +0600

    DZ3-2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
 ﻿Console.WriteLine("Введите число");
 int number_one = Convert.ToInt32(Console.ReadLine());
+int result = number_one % 2;
 
-Console.WriteLine("Введите число");
-int number_two = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите число");
-int number_three = Convert.ToInt32(Console.ReadLine());
-
-int Max = 0;
-
-if (number_one > number_two) Max = number_one;
-if (number_two > Max) Max = number_two;
-if (number_three > Max) Max = number_three;
-
-Console.Write(Max);
+if (result == 0) 
+{
+    Console.Write("Четное");
+}
+else
+{
+    Console.Write("Не четное");    
+}
